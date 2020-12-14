655ad239ac7aeacba216d860ba9c9b3b9dda2e19
Paulyne <paulynewambui2020@gmail.com>
2020-12-15 00:34:30 +0300

    created a gamers component

---
+++ src/app/gamers/gamers.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GamersComponent } from './gamers.component';
+
+describe('GamersComponent', () => {
+  let component: GamersComponent;
+  let fixture: ComponentFixture<GamersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GamersComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GamersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/gamers/gamers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gamers',
+  templateUrl: './gamers.component.html',
+  styleUrls: ['./gamers.component.css']
+})
+export class GamersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
