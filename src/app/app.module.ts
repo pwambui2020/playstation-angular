cdaaae751fdc4812b086785e4b016f277080a660
Paulyne <paulynewambui2020@gmail.com>
2020-12-15 00:41:33 +0300

    imported the components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TournamentComponent } from './tournament/tournament.component';
+import { GamersComponent } from './gamers/gamers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TournamentComponent,
+    GamersComponent
   ],
   imports: [
     BrowserModule,
