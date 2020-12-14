7e9ed01af9a5a9ea52568830efcf66e4957648ac
Paulyne <paulynewambui2020@gmail.com>
2020-12-15 00:39:57 +0300

    added the routing path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GamersComponent } from './gamers/gamers.component';
+import { HomeComponent } from './home/home.component';
+import { TournamentComponent } from './tournament/tournament.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch: "full",redirectTo: "/home"},
+  {path: "home", component: HomeComponent},
+  {path: "tournament", component: TournamentComponent},
+  {path: "gamers", component: GamersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
